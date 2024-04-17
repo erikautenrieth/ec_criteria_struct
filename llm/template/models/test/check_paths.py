f1be0bfa6a991a721a12729467bb68d234e57018
erikautenrieth <e-aut@web.de>
2024-04-17 12:57:35 +0200

    - add data

--- llm/template/models/test/check_paths.py
+++ llm/template/models/test/check_paths.py
@@ -1,7 +1,7 @@
 from llm.template.helper.helper_functions import read_text_file
 
 
-# export PYTHONPATH="${PYTHONPATH}:/home/eautenrieth/masterthesis/Masterthesis/llm"
+
 
 ec_data= "ec1"
 txt_path = f'llm/template/inputs/{ec_data}.txt'
