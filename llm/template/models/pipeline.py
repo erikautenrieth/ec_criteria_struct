7c3c4714c77d75c82ef0a1b36b3bdc9bba57511f
erikautenrieth <e-aut@web.de>
2024-04-25 17:12:22 +0200

    - structure

--- llm/template/models/pipeline.py
+++ llm/template/models/pipeline.py
@@ -2,26 +2,28 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from helper_functions import *
 
+
 torch.random.manual_seed(0)
 
 model = AutoModelForCausalLM.from_pretrained(
     "microsoft/Phi-3-mini-128k-instruct", 
-    device_map="cuda", 
+    device_map="cpu", # cuda
     torch_dtype="auto", 
     trust_remote_code=True, 
 )
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
 
 
-input1 = read_text_file("../input/c1.txt")
-ergebnis1 = load_json_string("../input/schema1.json")
-input2 = read_text_file("../input/c2.txt")
+schema1 = read_text_file("llm/template/input/s1.txt") # ../input/s1.txt
+label1 = load_json_string("llm/template/input/s1_label.json") #   ../input/s1_label.json
+
+input1 = read_text_file("llm/template/input/c1.txt") # ../input/c1.txt
 
 messages = [
-    {"role": "system", "content": "You return elegibility criterias of clinical studys in a logical form as json."},
-    {"role": "user", "content": f"Return me this criterias in a logical form as json: {input1}"},
-    {"role": "assistant", "content": ergebnis1},
-    {"role": "user", "content": f"Structure the following criterias as json: {input2}"},
+    {"role": "system", "content": "Create a JSON structure representing eligibility criteria for a clinical trial. Organize the criteria into inclusion (IC) and exclusion (EC) categories, each detailed with logical and numbered subcategories using AND, OR, and NOT operators.Return elegibility criterias of clinical studys in a logical form as json."},
+    {"role": "user", "content": f"Return me this criterias in a logical form as json: {schema1}"},
+    {"role": "assistant", "content": label1},
+    {"role": "user", "content": f"Structure the following criterias as json: {input1}"},
 ]
 
 
@@ -34,7 +36,7 @@ pipe = pipeline(
 generation_args = {
     "max_new_tokens": 700,
     "return_full_text": False,
-    "temperature": 0.0,
+    "temperature": 8.0,
     "do_sample": False,
 }
 
@@ -53,4 +55,7 @@ print(json_object)
 #print(pars_output)
 
 #save_json(json_object, "../output/phi3_c2.json")
-save_json_phi(gen_output, "../output/phi3_c2.json")
+save_txt(gen_output, "llm/template/output/phi3_c1.txt")
+
+save_json_phi(json_object, "llm/template/output/phi3_c1.json") # "../output/phi3_c2.json"
+
