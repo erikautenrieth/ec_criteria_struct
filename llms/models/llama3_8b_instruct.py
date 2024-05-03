0532f5d858ebea20d7dba15adb8f1d2c588d1a5c
erikautenrieth <e-aut@web.de>
2024-05-03 13:05:45 +0200

    - add llama3

--- llms/models/llama3_8b_instruct.py
+++ llms/models/llama3_8b_instruct.py
@@ -12,13 +12,14 @@ def main():
 
 
     # "meta-llama/Meta-Llama-3-8B" 
+    # "aaditya/Llama3-OpenBioLLM-8B"
     model_id =  "meta-llama/Meta-Llama-3-8B-Instruct" 
 
     pipeline = transformers.pipeline(
         "text-generation",
         model=model_id,
         model_kwargs={"torch_dtype": torch.bfloat16},
-        device="cpu", # cuda
+        device="cuda", # cuda
     )
 
     schema_input = read_text_file("llms/input/label_1/schema_0.txt")
