1c57a7e8aa74c094de7bf641090279f56e815eb3
erikautenrieth <e-aut@web.de>
2024-05-05 17:49:58 +0200

    - add sh file

---
+++ llms/models/llm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=gpu          # GPU partition
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --time=00:04:00          # Time limit hrs:min:sec
+
+#SBATCH --output=llms_pipeline.%j.out   # Standard output and error log
+#SBATCH --error=llms_pipeline.%j.err    # Error log
+
+module load cuda
+python llama3_8b.py
