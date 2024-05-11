22a667f2496d79c820eafbb3eaa8b5934dac3c2d
erikautenrieth <e-aut@web.de>
2024-05-11 16:07:15 +0200

    - structure

--- llms/models/code_llm_n_shot.py
+++ llms/models/code_llm_n_shot.py
@@ -1,19 +1,25 @@
-import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from helper_functions import *
 import time
 ## Studys
-study_path = "/work/eauten2s/ec_criteria_struct/datasets/Chia/transform_chia/input/studys/"
-output_path = "/work/eauten2s/ec_criteria_struct/datasets/Chia/transform_chia/eval/llama3_inst_3_shot/"
 
+#model_id =  "meta-llama/Meta-Llama-3-8B-Instruct"
+#model_name = "Llama-3-8B-Instruct"
+
+model_name  = "Nxcode_7B_orpo"
+model_id = "NTQAI/Nxcode-CQ-7B-orpo"
+
+study_path = "/work/eauten2s/ec_criteria_struct/datasets/Chia/transform_chia/input/studys/"
+output_path = f"/work/eauten2s/ec_criteria_struct/datasets/Chia/transform_chia/eval/{model_name}_3_shot/"
 
 
 INPUT_PATH = "/work/eauten2s/ec_criteria_struct/llms/input/chia/"
 OUTPUT_PATH = "/work/eauten2s/ec_criteria_struct/llms/output/chia/"
-model_name = "Llama-3-8B-Instruct"
 
-model_id =  "meta-llama/Meta-Llama-3-8B-Instruct"
 
-study_files = os.listdir(study_path)[20:100]   # mit LLama3 8B instruct bis [20:100]
+
+
+study_files = os.listdir(study_path)[:5]   # mit LLama3 8B instruct bis [20:100]
 
 model_desc = read_text_file(f"/work/eauten2s/ec_criteria_struct/llms/input/model_description.txt")
 s1 = read_text_file(INPUT_PATH+"NCT00050349_desc.txt")
@@ -23,12 +29,15 @@ l2 = read_text_file(INPUT_PATH+"NCT00061308.txt")
 s3 = read_text_file(INPUT_PATH+"NCT00094861_desc.txt")
 l3 = read_text_file(INPUT_PATH+"NCT00094861.txt")
 
-pipeline = transformers.pipeline(
-            "text-generation",
-            model=model_id,
-            model_kwargs={"torch_dtype": torch.bfloat16},
-            device_map="auto", 
-        )
+device = "cuda" # the device to load the model onto
+
+model = AutoModelForCausalLM.from_pretrained(
+    "NTQAI/Nxcode-CQ-7B-orpo",
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained("NTQAI/Nxcode-CQ-7B-orpo")
+
 
 for file in study_files:
     file_name = file.split("_")[0]
@@ -46,28 +55,10 @@ for file in study_files:
             {"role": "user", "content": f"bring the following study in json format with logical operators as in the example: {test_file}"},
         ]
 
-    prompt = pipeline.tokenizer.apply_chat_template(
-                messages, 
-                tokenize=False, 
-                add_generation_prompt=True
-    )
+    inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
+    outputs = model.generate(inputs, max_new_tokens=2000, do_sample=False, top_k=50, top_p=0.95, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
+    res = tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True)
 
-    terminators = [
-            pipeline.tokenizer.eos_token_id,
-            pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
-    ]
-
-
-    outputs = pipeline(
-            prompt,
-            max_new_tokens=2000,# 500 (LLama3), 256 (BIoLLama)
-            eos_token_id=terminators,
-            do_sample=True,
-            temperature=0.5,# 0.6 deterministich - kreativ
-            top_p=0.9,
-    )
-
-    gen_output = outputs[0]["generated_text"][len(prompt):]
-    print(f"\n {model_name} Output: \n  {gen_output} \n")
+    print(f"\n {model_name} Output: \n  {res} \n")
     
-    save_json_phi(gen_output, f"{output_path}{model_name}_{file_name}_3_shot.json")
+    save_json_phi(res, f"{output_path}{model_name}_{file_name}_3_shot.json")
