92805518c8ef6233492dbb81db0dc52169bc75c2
erikautenrieth <e-aut@web.de>
2024-05-14 17:34:58 +0200

    - data

--- llms/models/llm2.sh
+++ llms/models/llm2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=gpu4          # GPU partition
 #SBATCH --nodes=1                # number of nodes
-#SBATCH --mem=100G                # memory per node in MB (different units with suffix K|M|G|T)
+#SBATCH --mem=120G                # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --gres=gpu:4 
-#SBATCH --time=03:00:00          # Time limit hrs:min:sec
+#SBATCH --time=30:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=llms_pipeline2.%j.out   # Standard output and error log
-#SBATCH --error=llms_pipeline2.%j.err    # Error log
+#SBATCH --output=llms_pipeline3.%j.out   # Standard output and error log
+#SBATCH --error=llms_pipeline3.%j.err    # Error log
 #SBATCH --job-name=llm_pipeline
 
 module load cuda
