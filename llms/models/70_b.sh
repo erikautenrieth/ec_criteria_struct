09de9e3f1be6d8c9c7d265b1eda09ce221cee6b4
erikautenrieth <e-aut@web.de>
2024-05-14 20:06:12 +0200

    - data

--- llms/models/70_b.sh
+++ llms/models/70_b.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=250G                # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --gres=gpu:4 
-#SBATCH --time=35:00:00          # Time limit hrs:min:sec
+#SBATCH --time=20:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=llms_pipeline_70b.%j.out   # Standard output and error log
-#SBATCH --error=llms_pipeline_70b.%j.err    # Error log
-#SBATCH --job-name=llama3_70b
+#SBATCH --output=llms_pipeline_70b_inst.%j.out   # Standard output and error log
+#SBATCH --error=llms_pipeline_70b_inst.%j.err    # Error log
+#SBATCH --job-name=llama3_70b_inst
 
 module load cuda
 python llama3_70b_n_shot.py
