300f098b5ab5afb9dd98a77ffaf8755fadc99bdc
erikautenrieth <e-aut@web.de>
2024-05-16 10:32:29 +0200

    - files

--- models/llama3_70b.sh
+++ models/llama3_70b.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=gpu4          # GPU partition
-#SBATCH --nodes=1                # number of nodes
+#SBATCH --nodes=1                  # number of nodes
 #SBATCH --mem=250G                # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --gres=gpu:4 
-#SBATCH --time=30:00:00          # Time limit hrs:min:sec
+#SBATCH --time=50:00:00          # Time limit hrs:min:sec
 
 #SBATCH --output=log/llama3_70b_inst_5_shot.%j.out   # Standard output and error log
 #SBATCH --error=log/llama3_70b_inst_5_shot.%j.err    # Error log
 #SBATCH --job-name=llama3_70b_inst_5_shot
 
 module load cuda
-python llama3_70b_n_shot.py
+python llama3_70b_5_shot.py
 
 
   
