0f914d21fc9e420461f737e422ad817da320186d
erikautenrieth <e-aut@web.de>
2024-05-16 10:44:53 +0200

    - data

--- models/llama3_8b.sh
+++ models/llama3_8b.sh
@@ -5,11 +5,11 @@
 #SBATCH --gres=gpu:4 
 #SBATCH --time=20:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=log/llama3_8b_inst_5_shot.%j.out   # Standard output and error log
-#SBATCH --error=log/llama3_8b_inst_5_shot.%j.err    # Error log
-#SBATCH --job-name=llama3_8B_inst_5_shot
+#SBATCH --output=log/llama3_8b_inst_4_shot.%j.out   # Standard output and error log
+#SBATCH --error=log/llama3_8b_inst_4_shot.%j.err    # Error log
+#SBATCH --job-name=llama3_8B_inst_4_shot
 
 module load cuda
-python llama3_8b_5_shot_p2.py
+python llama3_8b_4_shot_p2.py
 
 
--- models/llama3_8b_4_shot_p2.py
+++ models/llama3_8b_4_shot_p2.py
@@ -8,7 +8,7 @@ transform_chia ="/work/eauten2s/ec_criteria_struct/transform_chia"
 model_id =  "meta-llama/Meta-Llama-3-8B-Instruct"
 model_name = "Llama-3-8B-Instruct"
 # N Shots
-n_shot = 5 # liefert genau die Anzahl Beispiele (study, label)
+n_shot = 4 # liefert genau die Anzahl Beispiele (study, label)
 
 # Input/ Output
 study_path = f"{transform_chia}/input/half_clinical_trials/"
