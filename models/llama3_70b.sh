5c29d2be2c8a50653ce9de9379cd1c88e9f8245a
erikautenrieth <e-aut@web.de>
2024-05-16 21:27:43 +0200

    - data

--- models/llama3_70b.sh
+++ models/llama3_70b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu4          # GPU partition
 #SBATCH --nodes=1                  # number of nodes
-#SBATCH --mem=250G                # memory per node in MB (different units with suffix K|M|G|T)
+#SBATCH --mem=260G                # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --gres=gpu:4 
 #SBATCH --time=50:00:00          # Time limit hrs:min:sec
 
