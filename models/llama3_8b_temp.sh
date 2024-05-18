50c944eac929b5b2434160a82f43b1c7952e0cbf
erikautenrieth <e-aut@web.de>
2024-05-18 15:21:38 +0200

    - data

--- models/llama3_8b_temp.sh
+++ models/llama3_8b_temp.sh
@@ -5,9 +5,9 @@
 #SBATCH --gres=gpu:4 
 #SBATCH --time=3:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=log/llama3_8b_inst_4_shot_temp9.%j.out   # Standard output and error log
-#SBATCH --error=log/llama3_8b_inst_4_shot_temp9.%j.err    # Error log
-#SBATCH --job-name=llama3_8b_inst_4_shot_temp_9
+#SBATCH --output=log/llama3_8b_inst_4_shot_temp1.%j.out   # Standard output and error log
+#SBATCH --error=log/llama3_8b_inst_4_shot_temp1.%j.err    # Error log
+#SBATCH --job-name=llama3_8b_inst_4_shot_temp_1
 
 module load cuda
 python llama3_8b_4_shot_temp.py
