62085f6f76776280171eb43dba5b53b09ef076e7
erikautenrieth <e-aut@web.de>
2024-05-18 15:29:09 +0200

    - data

--- models/llama3_8b_grad.sh
+++ models/llama3_8b_grad.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=260G                # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --gres=gpu:4 
-#SBATCH --time=30:00:00          # Time limit hrs:min:sec
+#SBATCH --time=03:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=log/llama3_8b_grad_20_shot.%j.out   # Standard output and error log
-#SBATCH --error=log/llama3_8b_grad_20_shot.%j.err    # Error log
-#SBATCH --job-name=llama3_8b_grad_20_shot
+#SBATCH --output=log/llama3_8b_grad_25_shot.%j.out   # Standard output and error log
+#SBATCH --error=log/llama3_8b_grad_25_shot.%j.err    # Error log
+#SBATCH --job-name=llama3_8b_grad_25_shot
 
 module load cuda
 python llama3_8b_grad_10_shot_p2.py
