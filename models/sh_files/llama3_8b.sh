c4959ea60520831f44a5d132c557ae8b6490b04f
erikautenrieth <e-aut@web.de>
2024-05-22 21:38:31 +0200

    - data

--- models/sh_files/llama3_8b.sh
+++ models/sh_files/llama3_8b.sh
@@ -5,11 +5,14 @@
 #SBATCH --gres=gpu:4 
 #SBATCH --time=3:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=log/llama3_8b_inst_4_shot.%j.out   # Standard output and error log
-#SBATCH --error=log/llama3_8b_inst_4_shot.%j.err    # Error log
-#SBATCH --job-name=llama3_8b_inst_4_shot
+SCRIPT_NAME="llama3_8b_4_shot_p1"
+
+#SBATCH --output=log/${SCRIPT_NAME}.%j.out   # Standard output and error log
+#SBATCH --error=log/${SCRIPT_NAME}.%j.err    # Error log
+#SBATCH --job-name=${SCRIPT_NAME}
 
 module load cuda
-python llama3_8b_4_shot_p2.py
+cd ..
+python ${SCRIPT_NAME}.py
 
 
