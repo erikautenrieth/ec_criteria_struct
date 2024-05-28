a6373c0833d1f7c4119469e5b34dedb1d6a6e108
erikautenrieth <e-aut@web.de>
2024-05-28 17:57:18 +0200

    - data

--- lct/models/falcon_lct.sh
+++ lct/models/falcon_lct.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=gpu4         # GPU partition
-#SBATCH --nodes=2                # number of nodes
+#SBATCH --nodes=3                # number of nodes
 #SBATCH --ntasks-per-node=4      # number of tasks per node
 #SBATCH --gres=gpu:4             # request 4 GPUs per node
 #SBATCH --mem=450G               # total memory for job
@@ -12,4 +12,4 @@
 module load cuda
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-python falcon.py
+python falcon_test.py
