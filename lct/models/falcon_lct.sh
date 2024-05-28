8861ceaa5c848e829c8e7d3a76a37c1aa8ae6c5e
erikautenrieth <e-aut@web.de>
2024-05-28 17:33:01 +0200

    - data

--- lct/models/falcon_lct.sh
+++ lct/models/falcon_lct.sh
@@ -3,12 +3,13 @@
 #SBATCH --nodes=2                # number of nodes
 #SBATCH --ntasks-per-node=4      # number of tasks per node
 #SBATCH --gres=gpu:4             # request 4 GPUs per node
-#SBATCH --mem=400G               # total memory for job
-#SBATCH --time=3:00:00           # Time limit hrs:min:sec
+#SBATCH --mem=450G               # total memory for job
+#SBATCH --time=00:20:00           # Time limit hrs:min:sec
 #SBATCH --output=log/falcon.%j.out   # Standard output and error log
 #SBATCH --error=log/falcon.%j.err    # Error log
 #SBATCH --job-name=falcon
 
 module load cuda
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python falcon.py
