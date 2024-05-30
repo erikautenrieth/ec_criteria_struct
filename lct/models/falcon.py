e88366a35d3fd3970ad0fcedf90413cb7e01046a
erikautenrieth <e-aut@web.de>
2024-05-30 11:53:34 +0200

    - data

--- lct/models/falcon.py
+++ lct/models/falcon.py
@@ -4,18 +4,35 @@ import torch
 
 model = "tiiuae/falcon-180b"
 
+
+prompt = "Insert the logical operators [AND], [OR], [NOT] into the following eligibility criteria and return the text in full without deleting/replacing anything:"
+text = """Inclusion criteria:
+  1. Age ≥ 18, male or female;
+  2. Subject must have had documented MM;
+  3. At screening phase, subject must have measurable disease;
+  4. Subject is in a state of progressive disease (PD);
+  5. Subject must have life expectancy of no less than 6 months;
+  6. Subject must have an ECOG (Eastern Cooperative Oncology Group) performance status score of 0~2;
+Exclusion criteria:
+  1. Subject has received anti-CD38 monoclonal antibody treatment previously;
+  2. Subject has received CAR-T cell therapy previously;
+  3. Subject has previously received allogenic stem cell transplant, or subject has received autologous stem cell transplant within 3 months before administration of the study agent;
+  4. Primary refractory multiple myeloma (subject failed to generate any minimal response or any degree of response to any therapy);
+  5. Subject has received anti-myeloma treatment (radiotherapy is excluded) within 4 weeks or 5 PK half-lives of the treatment, whichever longer, before the first study agent administration."""
+
+
 tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
-    torch_dtype=torch.float16, # torch.bfloat16
+    torch_dtype=torch.bfloat16, # torch.bfloat16
     trust_remote_code=True,
     device_map="auto",
 )
 sequences = pipeline(
-   "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
-    max_length=200,
+   prompt+text,
+    max_length=2000,
     do_sample=True,
     top_k=10,
     num_return_sequences=1,
