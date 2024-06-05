aca71ae934f631566a04e645e37c0e790cc92cae
erikautenrieth <e-aut@web.de>
2024-06-05 14:43:35 +0200

    - data

--- lct/models/lct.sh
+++ lct/models/lct.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #SBATCH --partition=gpu4         # GPU partition
 #SBATCH --nodes=1                # number of nodes
-#SBATCH --mem=100G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
+#SBATCH --mem=260G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
 #SBATCH --gres=gpu:4 
 #SBATCH --time=02:00:00          # Time limit hrs:min:sec
 
-#SBATCH --output=log/llama_shot.%j.out   # Standard output and error log
-#SBATCH --error=log/llama_shot.%j.err    # Error log
-#SBATCH --job-name=llama_shot
+#SBATCH --output=log/llm_shot.%j.out   # Standard output and error log
+#SBATCH --error=log/llm_shot.%j.err    # Error log
+#SBATCH --job-name=llm_k_shot
 
 module load cuda
-python phi3_p1_0_shot.py  # gpt2_0_shot_p1.py # phi3_p1_0_shot.py 
+python llama_p1_0_shot.py
+
+#llama_p1_0_shot.py # phi3_p1_0_shot.py  # gpt2_0_shot_p1.py # phi3_p1_0_shot.py 
 
 
