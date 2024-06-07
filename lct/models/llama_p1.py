74298189eb0f4144edbf3617d141f4a098e37aa2
erikautenrieth <e-aut@web.de>
2024-06-07 09:33:52 +0200

    - data

--- lct/models/llama_p1.py
+++ lct/models/llama_p1.py
@@ -2,11 +2,12 @@ import os
 import transformers
 from helper_functions import *
 
-batch_path = "eval_p1"
+batch_path = "eval_p1_n_shot"
 n_prompt = 7
-n_shot = 1
+n_shot = 5
 
 temp_str = ""
+cot_true = ""
 model_id =  "meta-llama/Meta-Llama-3-70B-Instruct"
 model_name = "Llama-3-70B-Instruct"
 
@@ -23,7 +24,7 @@ transform_lct ="/work/eauten2s/ec_criteria_struct/lct"
 model_desc = read_text_file(f"{transform_lct}/input/prompt/p{n_prompt}.txt")
 
 study_path = f"{transform_lct}/input/lct_txt/"
-output_path = f"{transform_lct}/evaluate/{batch_path}/model_output/{model_name}_{n_shot}_shot_prompt_{n_prompt}{temp_str}_cot/output/"
+output_path = f"{transform_lct}/evaluate/{batch_path}/model_output/{model_name}_{n_shot}_shot_prompt_{n_prompt}{temp_str}{cot_true}/output/"
 os.makedirs(output_path, exist_ok=True)
 
 
@@ -37,19 +38,17 @@ shot_list = [
     "NCT03930121.txt"
 ]
 
-one_shot_list = ["NCT03923231.txt"]
-
 # Load n-shot Data
 study_folder = f"{transform_lct}/input/lct_txt/"
 label_folder = f'{transform_lct}/input/lct_p1'
-study_filenames, study_contents, label_filenames, label_contents = read_matching_txt_files(study_folder, label_folder, one_shot_list)
+study_filenames, study_contents, label_filenames, label_contents = read_matching_txt_files(study_folder, label_folder, shot_list)
 studies = dict(zip(study_filenames, study_contents))
 labels = dict(zip(label_filenames, label_contents))
 messages = []
 
 cot = "Let's think through this carefully, step by step."
 
-command = "Insert the logical operators [AND], [OR], [NOT] into the following eligibility criteria and return the text in full without deleting/replacing anything:" + cot
+command = "Insert the logical operators [AND], [OR], [NOT] into the following eligibility criteria and return the text in full without deleting/replacing anything. Do not say anything else." 
 
 messages.append({"role": "system", "content": f"{model_desc}"})
 
