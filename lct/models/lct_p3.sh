ae423b1d22a6a76a37d499b9dd302d9b93b62efa
erikautenrieth <e-aut@web.de>
2024-06-14 11:05:37 +0200

    - data

--- lct/models/lct_p3.sh
+++ lct/models/lct_p3.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=260G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
 #SBATCH --gres=gpu:4 
-#SBATCH --time=8:00:00          # Time limit hrs:min:sec
+#SBATCH --time=25:00:00          # Time limit hrs:min:sec
 
 #SBATCH --output=log/llama_p3.%j.out   # Standard output and error log
 #SBATCH --error=log/llama_p3.%j.err    # Error log
