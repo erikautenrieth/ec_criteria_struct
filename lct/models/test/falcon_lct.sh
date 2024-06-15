304d26e8f2e0308cfd4c47a70ffbf5f3bacf8b16
erikautenrieth <e-aut@web.de>
2024-06-15 14:09:31 +0200

    - data

--- lct/models/test/falcon_lct.sh
+++ lct/models/test/falcon_lct.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition=gpu4         # GPU partition
 #SBATCH --nodes=3                # number of nodes
-#SBATCH --ntasks-per-node=4      # number of tasks per node
-#SBATCH --gres=gpu:4             # request 4 GPUs per node
+#SBATCH --ntasks-per-node=8      # number of tasks per node
+#SBATCH --gres=gpu:8             # request 4 GPUs per node
 #SBATCH --mem=450G               # total memory for job
 #SBATCH --time=00:40:00          # Time limit hrs:min:sec
 #SBATCH --output=log/falcon.%j.out   # Standard output and error log
