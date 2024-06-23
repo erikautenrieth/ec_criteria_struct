5c2cd8b9368d7d5c9c69ecc6ef48a06acd7ea491
erikautenrieth <e-aut@web.de>
2024-06-23 09:38:18 +0200

    - data

--- lct/models/finetuning/finetune_llama3_p1.py
+++ lct/models/finetuning/finetune_llama3_p1.py
@@ -30,7 +30,7 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 )
 model = FastLanguageModel.get_peft_model(
     model,
-    r = 128, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
                       "gate_proj", "up_proj", "down_proj",],
     lora_alpha = 16,
