c54bc7ebab80a12a5e43579c4a56d0658d5c39c0
erikautenrieth <e-aut@web.de>
2024-06-25 11:06:13 +0200

    - data

--- lct/models/llama_p1.py
+++ lct/models/llama_p1.py
@@ -2,7 +2,7 @@ import os
 import transformers
 from helper_functions import *
 
-batch_path = "eval_p1_5_shot_temperatur"
+batch_path = "eval_p1_n_shot_modelle_prompt6"
 n_prompt = 6
 n_shot = 5
 
@@ -37,15 +37,15 @@ model_name = "Llama-3-70B-Instruct"
 #model_id = "aaditya/OpenBioLLM-Llama3-70B"
 #model_name = "OpenBioLLM-Llama3-70B"
 
-temp = 0.9
+temp = 0.8
 transform_lct ="/work/eauten2s/ec_criteria_struct/lct"
 
 
 model_desc = read_text_file(f"{transform_lct}/input/prompt/p{n_prompt}.txt") 
 
 study_path = f"{transform_lct}/input/lct_txt/"
-#output_path = f"{transform_lct}/evaluate/{batch_path}/model_output/{model_name}_{n_shot}_shot/output/"  #_prompt_{n_prompt}{temp_str}{cot_true}
-output_path = f"{transform_lct}/evaluate/{batch_path}/model_output/Temperatur {str(temp)}/output/"
+output_path = f"{transform_lct}/evaluate/{batch_path}/model_output/{model_name}_{n_shot}_shot/output/"  #_prompt_{n_prompt}{temp_str}{cot_true}
+#output_path = f"{transform_lct}/evaluate/{batch_path}/model_output/Temperatur {str(temp)}/output/"
 
 os.makedirs(output_path, exist_ok=True)
 
