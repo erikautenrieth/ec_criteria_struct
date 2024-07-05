d21472ecbfcca9a9bd3c59139d6a5f4963779a72
erikautenrieth <e-aut@web.de>
2024-07-05 09:28:50 +0200

    - data

--- chia/models/llama_lct_p1_tuned.py
+++ chia/models/llama_lct_p1_tuned.py
@@ -6,20 +6,21 @@ from helper_functions import *
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 from unsloth import FastLanguageModel
 
-batch_path = "eval_p1_finetuned_testset_prompt6_llama3_70b" #"eval_p1_finetuned_testset_prompt1_llama3_70b" #"eval_p1_finetuned_testset"
+batch_path = "eval_p1_finetuned" 
 n_prompt = 1
-#model_name = "llama3_8b_lora_model_ep10"
 
-model_id = "../../lct/models/tuned_models/llama3_70b_Lora_ep30_256_prompt6_evalset"     #llama3_70b_Lora_ep20_128_prompt6_evalset #llama3_70b_lora_ep10_prompt1
-model_name = "Llama3_70b_Lora_ep30_prompt6_evalset"
-  
+model_id = "../../lct/models/tuned_models/llama3_70b_Lora_ep30_256_prompt6_evalset"   
+model_name = "Llama3_70b_Lora_ep30_prompt6"
+print(f"Modellpfad: {model_id}")
+print(f"Existiert: {os.path.exists(model_id)}")
+print(f"Inhalt des Verzeichnisses: {os.listdir(model_id)}")  
 transform ="/work/eauten2s/ec_criteria_struct/chia"
-model_desc = read_text_file(f"{transform}/input/prompt/p{n_prompt}.txt")
+model_desc = read_text_file(f"{transform}/input/prompt/lct_p2.txt")
 
 study_path = f"{transform}/input/chia_text_half/"
-output_path = f"{transform}/evaluate/{batch_path}/model_output/{model_name}_prompt_{n_prompt}/output/"
+output_path = f"{transform}/evaluate/{batch_path}/model_output/{model_name}_prompt_lct2/output/"
 os.makedirs(output_path, exist_ok=True)
-study_files = os.listdir(study_path)
+study_files = os.listdir(study_path)[3:100]
 
 model, tokenizer = FastLanguageModel.from_pretrained(
         model_name = model_id,
@@ -27,8 +28,8 @@ model, tokenizer = FastLanguageModel.from_pretrained(
         dtype = None,
         load_in_4bit = True,
     )
-FastLanguageModel.for_inference(model) # Enable native 2x faster inference
 
+FastLanguageModel.for_inference(model)
 
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
@@ -41,8 +42,6 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 ### Response:
 {}"""
 
-command = read_text_file(f"{transform_lct}/input/prompt/p1.txt")
-
 first_call = True
 
 for file in study_files:
@@ -54,13 +53,13 @@ for file in study_files:
     inputs = tokenizer(
     [
         alpaca_prompt.format(
-            f"{command}", # instruction
+            f"{model_desc}", # instruction
             f"{test_file}", # input
             "", # output - leave this blank for generation!
         )
     ], return_tensors = "pt").to("cuda")
 
-    outputs = model.generate(**inputs, max_new_tokens=2048, use_cache=True) #  temperature=0.5
+    outputs = model.generate(**inputs, max_new_tokens=2048) #  temperature=0.5, use_cache=True
     decoded_outputs = tokenizer.batch_decode(outputs)
     response = decoded_outputs[0].split("### Response:")[1].strip()
     response = response.replace("<|eot_id|>", "")
