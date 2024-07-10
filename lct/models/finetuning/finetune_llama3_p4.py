914f2ca3cd0142714a4abe3fcc082109988c6a15
erikautenrieth <e-aut@web.de>
2024-07-10 13:30:48 +0200

    - data

--- lct/models/finetuning/finetune_llama3_p4.py
+++ lct/models/finetuning/finetune_llama3_p4.py
@@ -88,7 +88,7 @@ trainer = SFTTrainer(
         weight_decay = 0.01,
         lr_scheduler_type = "linear",
         seed = 3407,
-        output_dir = "outputs",
+        output_dir = "outputs_p4",
     ),
 )
 
