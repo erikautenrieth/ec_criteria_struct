1b95dc12445ce7c03d8c63f69e0184a4791b6717
erikautenrieth <e-aut@web.de>
2024-07-11 09:33:29 +0200

    - data

--- lct/models/lct.sh
+++ lct/models/lct.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=260G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
 #SBATCH --gres=gpu:4 
-#SBATCH --time=72:00:00          # Time limit hrs:min:sec
+#SBATCH --time=40:00:00          # Time limit hrs:min:sec
 
 #SBATCH --output=log/llm_shot.%j.out   # Standard output and error log
 #SBATCH --error=log/llm_shot.%j.err    # Error log
-#SBATCH --job-name=llm_2_shot
+#SBATCH --job-name=llm_1_shot
 
 module load cuda
 python llama_p1_eval_shot.py
