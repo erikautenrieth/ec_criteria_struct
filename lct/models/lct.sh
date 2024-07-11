b69455847991a0cbb06b7f8d7893e7e374471ccd
erikautenrieth <e-aut@web.de>
2024-07-11 08:21:56 +0200

    - data

--- lct/models/lct.sh
+++ lct/models/lct.sh
@@ -3,14 +3,14 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=260G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
 #SBATCH --gres=gpu:4 
-#SBATCH --time=04:00:00          # Time limit hrs:min:sec
+#SBATCH --time=72:00:00          # Time limit hrs:min:sec
 
 #SBATCH --output=log/llm_shot.%j.out   # Standard output and error log
 #SBATCH --error=log/llm_shot.%j.err    # Error log
-#SBATCH --job-name=llm_n_shot
+#SBATCH --job-name=llm_2_shot
 
 module load cuda
-python llama_p1_tuned.py
+python llama_p1_eval_shot.py
 #gemma_p1_5_shot.py
 # llama_p1_0_shot.py # phi3_p1_0_shot.py  # gpt2_0_shot_p1.py # phi3_p1_0_shot.py  llama_p1.py
 # llama_p1_0_shot_replace.py llama_autocriteria.py qwen_p1.py  llama_p1_tuned.py
--- lct/models/llama_p1_eval_shot.py
+++ lct/models/llama_p1_eval_shot.py
@@ -39,7 +39,7 @@ pipeline = transformers.pipeline(
             )
 
 
-for i in range(3, 4):
+for i in range(2, 3):
     batch_path = f"eval_n_shot/eval_{i}_shot"
     n_shot = i
     
