11573f29b375beffa6bf5bd4ac49daed959f033d
erikautenrieth <e-aut@web.de>
2024-07-14 09:11:23 +0200

    - data

--- lct/models/llama_p1_eval_shot.py
+++ lct/models/llama_p1_eval_shot.py
@@ -39,11 +39,11 @@ pipeline = transformers.pipeline(
             )
 
 
-for i in range(1, 2):
+for i in range(5, 6):
     batch_path = f"eval_n_shot/eval_{i}_shot"
     n_shot = i
     
-    for j in range(21, 22):
+    for j in range(16, 22):
         study_path = f"{transform_lct}/input/dataset/test/input/"
         output_path = f"{transform_lct}/evaluate_parse_1/{batch_path}/model_output/{n_shot}_shot_v_{j}/output/"  
         os.makedirs(output_path, exist_ok=True)
