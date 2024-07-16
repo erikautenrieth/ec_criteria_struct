3edc0500a8739a6fda580249fba973ebd8e32f0b
erikautenrieth <e-aut@web.de>
2024-07-16 18:20:44 +0200

    - data

--- lct/models/claude_sonnet_5_shot_p1.py
+++ lct/models/claude_sonnet_5_shot_p1.py
@@ -3,23 +3,19 @@ from helper_functions import *
 import os
 ## Kosten 100 Files: ca. 3€
 
-batch_path = "eval_p1_claude"
+batch_path = "eval_p1_models_prompt6_evaldata"
 n_prompt = 6
 n_shot = 5
 
 model_name = "Claude-3.5-Sonnet" # "Claude-3-Opus"
 
 transform_lct ="/work/eauten2s/ec_criteria_struct/lct"
-model_desc = read_text_file(f"{transform_lct}/input/prompt/claude_prompt.txt") # claude_prompt.txt (p6 mit 5 Beispielen)
-study_path = f"{transform_lct}/input/lct_txt/"
-output_path = f"{transform_lct}/evaluate_parse_1/{batch_path}/model_output/{model_name}_{n_shot}_shot_temp_0.0/output/"
+model_desc = read_text_file(f"{transform_lct}/input/prompt/claude_prompt6.txt") 
+study_path = f"{transform_lct}/input/dataset/test/input/"
+output_path = f"{transform_lct}/evaluate_parse_1/{batch_path}/model_output/{model_name}_{n_shot}_shot_temp_0.5/output/"
 os.makedirs(output_path, exist_ok=True)
 
-study_files = os.listdir(study_path)[0:50]
-
-
-study_folder = f"{transform_lct}/input/lct_txt/"
-label_folder = f'{transform_lct}/input/lct_p1'
+study_files = os.listdir(study_path)
 
 client = anthropic.Anthropic(
     api_key="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",#os.getenv("ANTHROPIC_API_KEY"), #os.environ.get("ANTHROPIC_API_KEY"),
@@ -33,7 +29,7 @@ for file in study_files:
     message = client.messages.create(
         model="claude-3-5-sonnet-20240620",  # "claude-3-opus-20240229",
         max_tokens=3000,
-        temperature=0,
+        temperature=0.5,
         system=model_desc,
         messages=[
             {
