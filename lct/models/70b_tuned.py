422ac659491a8683760c29ba1502efc48cb7f1c6
erik <e-aut@web.de>
2024-07-22 10:52:53 +0200

    - add lct

--- lct/models/70b_tuned.py
+++ lct/models/70b_tuned.py
@@ -56,7 +56,7 @@ for file in study_files:
         )
     ], return_tensors = "pt").to("cuda")
 
-    outputs = model.generate(**inputs, max_new_tokens=2048, use_cache=True) #  temperature=0.5
+    outputs = model.generate(**inputs, max_new_tokens=2048, use_cache=True, temperature=1.0) #  temperature=0.5
     decoded_outputs = tokenizer.batch_decode(outputs)
     response = decoded_outputs[0].split("### Response:")[1].strip()
     response = response.replace("<|eot_id|>", "")
