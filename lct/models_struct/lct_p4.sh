2e1abb3c8c701dfd95a4b115eddc11ebe98b1241
erikautenrieth <e-aut@web.de>
2024-09-01 13:57:58 +0200

    - data

--- lct/models_struct/lct_p4.sh
+++ lct/models_struct/lct_p4.sh
@@ -3,14 +3,14 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=260G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
 #SBATCH --gres=gpu:4 
-#SBATCH --time=25:00:00          # Time limit hrs:min:sec
+#SBATCH --time=40:00:00          # Time limit hrs:min:sec
 
 #SBATCH --output=log/llama_p4.%j.out   # Standard output and error log
 #SBATCH --error=log/llama_p4.%j.err    # Error log
 #SBATCH --job-name=llama_struct
 
 module load cuda
-python llama3_p4.py #
+python qwen2_p4.py # llama3_p4.py
 
 
 
--- lct/models_struct/qwen2_p4.py
+++ lct/models_struct/qwen2_p4.py
@@ -8,7 +8,7 @@ from helper_functions import *
 
 
 batch_path = "eval_p4"
-n_shot = 2
+n_shot = 25
 
 model_id =  "Qwen/Qwen2-72B-Instruct"
 model_name = "Qwen2-72B"
@@ -20,7 +20,7 @@ model_desc = read_text_file(f"{transform_lct}/input/prompt/all_entitys_prompt1.t
 command = "Structure the eligibility criteria based on the system input in JSON and extract the entities."
 study_path = f"{transform_lct}/input/dataset_p4_prompt1_new/test/input/"
 
-output_path = f"{transform_lct}/evaluate_struct/{batch_path}/model_output/{model_name}_2_Shot_short_files/output/"
+output_path = f"{transform_lct}/evaluate_struct/{batch_path}/model_output/{model_name}_{str(n_shot)}_Shot_short_files/output/"
 os.makedirs(output_path, exist_ok=True)
 
 study_files = os.listdir(study_path)
@@ -46,9 +46,31 @@ top_files = [
 short_files = [
 "NCT03860714_inc.txt",
 "NCT03860012_exc.txt",
-"NCT03860090_exc.txt"
+"NCT03860090_exc.txt",
+"NCT03863756_exc.txt",
+"NCT03926949_inc.txt",
+"NCT03868865_exc.txt",
+"NCT03867422_inc.txt",
+"NCT03864653_exc.txt",
+"NCT03922269_inc.txt",
+"NCT03921138_exc.txt",
+'NCT03860025_inc.txt',
+'NCT03861221_inc.txt',
+'NCT03861286_inc.txt',
+'NCT03860779_inc.txt',
+'NCT03860493_exc.txt',
+'NCT03861689_inc.txt',
+'NCT03863223_inc.txt',
+'NCT03861078_exc.txt',
+'NCT03863548_inc.txt',
+'NCT03863873_inc.txt',
+'NCT03864315_inc.txt',
+'NCT03864549_inc.txt',
+'NCT03864874_exc.txt',
+'NCT03864770_inc.txt',
+'NCT03864094_inc.txt',
+'NCT03864991_inc.txt'
 ]
-
 study_folder = f"{transform_lct}/input/dataset_p4_prompt1_new/train/input/"
 label_folder = f"{transform_lct}/input/dataset_p4_prompt1_new/train/output/"
 
