41bfcd2636c360fb40616aa12c0269a9666efb21
erik <e-aut@web.de>
2024-10-13 21:28:27 +0200

    - add lct

---
+++ lct/finetuning/finetune_llama3_70b_p4.py
@@ -0,0 +1,134 @@
+from unsloth import FastLanguageModel
+from trl import SFTTrainer
+from transformers import TrainingArguments
+import torch
+import os
+from datasets import load_from_disk
+
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+
+r = 256
+epoch = 10
+
+output_dir  = "outputs/outputs_8b_2" # outputs_70b_p4_5
+os.makedirs(output_dir, exist_ok=True)
+
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "meta-llama/Meta-Llama-3-70B-Instruct", #"unsloth/llama-3-8b-Instruct-bnb-4bit",   # meta-llama/Meta-Llama-3-8B-Instruct
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    token = "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"# token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = r, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096 (zu groß)
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 256, # 256 (default),
+    lora_dropout=0.05,
+    bias = "none",    # Supports any, but = "none" is optimized
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context  # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    random_state = 3407,
+    use_rslora = True,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+
+## 80/20 Training 904 Dokumente Training, 202 Test Set [Random]
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+EOS_TOKEN = tokenizer.eos_token 
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
+pass
+
+dataset_path = 'dataset/dataset_p4_prompt1'
+dataset = load_from_disk(dataset_path)
+train_test_split = dataset['train'].train_test_split(test_size=0.05, seed=42)
+train = train_test_split['train'] # 723 Files
+test = train_test_split['test'] # 81 Files
+train = train.map(formatting_prompts_func, batched=True)
+test = test.map(formatting_prompts_func, batched=True)
+
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = train,
+    eval_dataset = test,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 4,  
+    packing = True,  
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,  
+        gradient_accumulation_steps = 8,  
+        per_device_eval_batch_size = 4,  
+        num_train_epochs = epoch,  
+        warmup_ratio = 0.1,  
+        learning_rate = 5e-5, 
+        fp16 = not torch.cuda.is_bf16_supported(),
+        bf16 = torch.cuda.is_bf16_supported(),
+        optim = "adamw_8bit",
+        weight_decay = 0.05,  
+        lr_scheduler_type = "cosine",  # cosine (default)
+        seed = 42, 
+        output_dir = output_dir,
+        logging_steps = 20,  
+        evaluation_strategy = 'steps',  
+        eval_steps = 20,  # 100
+        save_strategy = 'steps',  
+        save_steps = 20,  # 100
+        load_best_model_at_end = True,
+        metric_for_best_model = "eval_loss", 
+        greater_is_better = False,  
+        group_by_length = True,  
+        gradient_checkpointing = True,  
+        max_grad_norm = 1.0,  
+    ),
+)
+
+
+#@title Show current memory stats
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+
+trainer_stats = trainer.train()
+
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory         /max_memory*100, 3)
+lora_percentage = round(used_memory_for_lora/max_memory*100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training.")
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+
+model.save_pretrained(f"70b_p4/llama3_70b_Lora_ep{epoch}_r{r}_prompt1_20eval_5pct_p4") 
---
+++ lct/finetuning/finetuning.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=gpu4         # GPU partition
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --mem=260G               # memory per node in MB (different units with suffix K|M|G|T) 260-70B, 160-8B
+#SBATCH --gres=gpu:4 
+#SBATCH --time=72:00:00          # Time limit hrs:min:sec
+#SBATCH --output=8b_tunep4.%j.out   # Standard output and error log
+#SBATCH --error=8b_tunep4.%j.err    # Error log  p1_8b_e10_r2048.%j.err 
+#SBATCH --job-name=8b_tune
+
+module load cuda
+export CUDA_VISIBLE_DEVICES=0 
+
+python finetune_llama3_8b_p4.py #finetune_llama3_70b_p1_train.py #finetune_llama3_70b_p4.py
+
+
+
+  ##SBATCH --nodelist=wr24          # Specify the node 20 (fail), 21, (geht) 22 (geht), 23 (geht), 24 (fail) ,25 (geht) !
